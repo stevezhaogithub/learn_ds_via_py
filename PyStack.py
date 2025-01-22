9233ccd2f11297e2efe2ad34fccac07c412e47a3
steve xiaohu zhao <steve_zhao@163.com>
2025-01-22 11:34:51 +0800

    add test code in PyStack.py

--- PyStack.py
+++ PyStack.py
@@ -35,33 +35,3 @@ class PyStack:
     def size(self):
         return len(self.items)
 
-
-# ======================= 以下为测试 PyStack 代码 ===============
-
-# 1. 创建一个空栈
-stack = PyStack()
-
-#  2. 输出判断栈是否为空
-print('stack 是否为空：' + stack.is_empty())
-
-# 3. 入栈
-stack.push('steve')
-stack.push(100)
-stack.push('李四')
-
-# 4. peek() 以下
-print('stack.peek() ' + stack.peek())
-stack.push(True)
-
-# 5. 输出栈大小
-print(stack.size())
-print(stack.is_empty())
-
-stack.push(8.4)
-
-print(stack.pop())
-print(stack.pop())
-print(stack.pop())
-
-# 最后再输出栈大小
-print(stack.size())
---
+++ test_code.py
@@ -0,0 +1,87 @@
+#
+#  test_code.py
+#  pythonProject4
+#
+#  Created by Z. Steve on 2025/1/22 10:54.
+#
+
+
+from PyStack import PyStack
+
+
+# ======================= 以下为测试 PyStack 代码 ===============
+
+# 1. 测试基本栈操作
+def test():
+    # 1. 创建一个空栈
+    stack = PyStack()
+
+    #  2. 输出判断栈是否为空
+    print('stack 是否为空：{}'.format(stack.is_empty()))
+
+    # 3. 入栈
+    stack.push('steve')
+    stack.push(100)
+    stack.push('李四')
+
+    # 4. peek() 以下
+    print('stack.peek() {} '.format(stack.peek()))
+    stack.push(True)
+
+    # 5. 输出栈大小
+    print(stack.size())
+    print(stack.is_empty())
+
+    stack.push(8.4)
+
+    print(stack.pop())
+    print(stack.pop())
+    print(stack.pop())
+
+    # 最后再输出栈大小
+    print(stack.size())
+
+
+# 判断给定的两个括号是否为同一类型括号
+def is_match(left, right):
+    lbs = '([{'
+    rbs = ')]}'
+    return lbs.index(left) == rbs.index(right)
+
+
+# 2. 栈的应用，判断表达式中括号是否成对出现
+def bracket_check(strcode):
+    # 1. 创建一个空栈
+    stack = PyStack()
+    # 2. 声明一个标记变量，默认表示字符串中的括号是成对出现的
+    balanced = True
+    index = 0
+
+    # 开始循环判断
+    while index < len(strcode) and balanced:
+        # 取出当前遍历的字符
+        symbol = strcode[index]
+        if symbol in '([{':
+            stack.push(symbol)
+        else:
+            if stack.is_empty():
+                balanced = False
+            else:
+                top = stack.pop()
+                if not is_match(top, symbol):
+                    balanced = False
+        index = index + 1
+    if balanced and stack.is_empty():
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    # 1. 调用 test() 测试方法
+    # test()
+
+    # 2. 调用测试括号是否成对出现
+    print(bracket_check('{[]}'))
+    print(bracket_check('()()(){}{}[]'))
+    print(bracket_check('([)]'))
