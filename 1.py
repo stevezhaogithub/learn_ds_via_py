50b929d0e97b666b10b5c4247023b30008f8ffcd
steve xiaohu zhao <steve_zhao@163.com>
2025-01-22 19:24:28 +0800

    add test code in PyStack.py

--- 1.py
+++ 1.py
@@ -6,5 +6,14 @@
 #
 
 
-def test():
-    print("test")
+# print('ISBN号码是：', 10087, '对没错！！')
+# print('100 + 200 = ', 100 + 200)
+
+
+print('请输入姓名：')
+name = input()
+print('请输入年龄：')
+age = input()
+
+print('你的姓名：', name)
+print('你的年龄：', age)
